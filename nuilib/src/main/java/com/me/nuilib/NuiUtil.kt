3feea17abd6efdd7711abf0223725aa4305c9799
zq <472378168@qq.com>
2024-04-17 10:57:00 +0800

    首次提交

---
+++ nuilib/src/main/java/com/me/nuilib/NuiUtil.kt
@@ -0,0 +1,13 @@
+package com.me.nuilib
+
+/**
+ * @CreateTime : 2024/4/17 10:53
+ * @Author : zq
+ * @Description :
+ */
+class NuiUtil {
+    fun getName(): String{
+        return "洗洗睡吧"
+    }
+
+}
